7d2b81aed704b1e67a362c620fc71059d6c6ee16
JoaoPaulo <perseu456@gmail.com>
2019-05-20 00:00:57 -0300

    #6 - [FE] Definir Layout base no React

--- uati-react/src/components/login.js
+++ uati-react/src/components/login.js
@@ -1,12 +1,9 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
 
 
 
@@ -18,28 +15,35 @@ export default class Login extends React.Component {
         return (
             <div className="LoginContainer">
                 <div className="LoginBox">
-                    <center>
-                        <h1 className="LoginTitle">Login</h1>
-                        <TextField
-                            id="standard-with-placeholder"
-                            label="Username"
-                            placeholder="userName"
-                            style = {{width: 250}} 
-                            margin="normal"
-                        />
-                        <br></br>
-                        <TextField
-                            id="outlined-password-input"
-                            label="Password"
-                            type="password"
-                            autoComplete="current-password"
-                            style = {{width: 250}} 
-                            margin="normal"
+                    <div className="Fields">
+                        <center>
+                            <h1 className="LoginTitle">Login</h1>
+                            <TextField
+                                id="standard-with-placeholder"
+                                label="Username"
+                                placeholder="userName"
+                                style={{ width: 250 }}
+                                margin="normal"
+                            />
+                            <br></br>
+                            <TextField
+                                id="outlined-password-input"
+                                label="Password"
+                                type="password"
+                                autoComplete="current-password"
+                                style={{ width: 250 }}
+                                margin="normal"
+                            />
+                        </center>
+                    </div>
+                    <div className="Buttons">
+                        <center>
+                            <Button variant="contained" color="primary" >Enter</Button>
+                            <Button variant="contained" color="primary" style={{ marginLeft: 10 }} >
+                                register</Button>
+                        </center>
+                    </div>
 
-                            
-                        />
-
-                    </center>
                 </div>
             </div>
 
