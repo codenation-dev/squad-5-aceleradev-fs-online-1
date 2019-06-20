99fc5e3672d55ffba4cca08ea5d811e1fb591c50
rafael mendes sienna <rmsphd@gmail.com>
2019-06-20 17:12:40 -0300

    Ajustes contratos e documentação swagger

--- api/src/domain/model/alert.go
+++ api/src/domain/model/alert.go
@@ -10,7 +10,7 @@ type Alert struct {
 	Type          AlertType    `json:"type" xorm:"int notnull"`
 	Description   string       `json:"description" xorm:"varchar(100) notnull"`
 	Customer      *Customer    `json:"customer,omitempty" xorm:"customer_id varchar(26) index null"`
-	PublicAgent   *PublicAgent `json:"publicAgent,omitempty" xorm:"public_agent_id varchar(26) index null"`
+	PublicAgent   *PublicAgent `json:"public_agent,omitempty" xorm:"public_agent_id varchar(26) index null"`
 	User          *User        `json:"user,omitempty" xorm:"user_id varchar(26) index null"`
 	UsersReceived []User       `json:"users_received,omitempty" xorm:"-"`
 	CreatedAt     time.Time    `json:"datetime" xorm:"notnull created"`
